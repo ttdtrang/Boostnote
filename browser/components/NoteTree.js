86bab9e78843a5c2897a7c50e31796a70375acfd
Trang Tran <trang@Jupiter>
2018-02-18 22:06:42 -0700

    tree shown with button

--- browser/components/NoteTree.js
+++ browser/components/NoteTree.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import CSSModules from 'browser/lib/CSSModules'
 import styles from './NoteTree.styl'
+import { Subscription } from 'rx';
 
 /**
  * @description Note item component when using simple display mode.
@@ -19,9 +20,19 @@ class NoteTree extends React.Component {
   constructor (props) {
     super(props)
     this.noteTreeData = props.noteTreeData
-
+    this.state = this.initializeState(props.noteTreeData) 
   }
-
+  initializeState (treeObj) {
+    let stateTree
+    Object.keys(treeObj).map(k =>  {
+      if (Array.isArray(treeObj[k])) {
+        stateTree[k] = {'isOpen': false, children = treeObj[k]}
+      } else {
+        stateTree[k] = this.initializeState(treeObj[k])
+      }
+    })
+    this.setState({stateTree})
+  }
   renderTreeAsList (treeObj) {
     const treeDisplay = Object.keys(treeObj).map((k) => {
       let children
@@ -42,7 +53,7 @@ class NoteTree extends React.Component {
       }
       return (
         <li>{k}
-        { children } 
+          { children }
         </li>
       )
     })
@@ -54,9 +65,14 @@ class NoteTree extends React.Component {
   }
 
 render () {
+  const root = Object.keys(this.props.noteTreeData).map(k => {
+    return (
+      <TreeNode treeData={this.props.noteTreeData[k]} label={k}/>
+    )
+  })
   return (
     <div className='NoteTree' styleName='tree'>
-    {this.renderTreeAsList(this.noteTreeData)}
+      {root}    
     </div>
   )
 }
@@ -75,5 +91,76 @@ render () {
 //   handleNoteContextMenu: PropTypes.func.isRequired,
 //   handleDragStart: PropTypes.func.isRequired
 // }
+
+class TreeNode extends React.PureComponent {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isOpen: false 
+    }
+  }
+
+  handleToggleButtonClick (currentState,keychain) {
+    console.log(e.target)
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+
+  renderNode (label, subTree) {
+    const subTreeDisplay = (Array.isArray(subTree)) ? 
+      subTree.map(item =>  {
+        return ( <li>{item.title}</li>)
+      })
+    : Object.keys(subTree).map(k => {
+        return ( this.renderNode(k, subTree[k]) )
+        })
+    return (
+      <ol>
+          <button styleName='header-toggleButton'
+                onMouseDown={e => {this.handleToggleButtonClick(e)}}
+              >
+                <img src={this.state.isOpen
+                  ? '../resources/icon/icon-down.svg'
+                  : '../resources/icon/icon-right.svg'
+                  }
+                />
+          </button>
+                {label}
+        {this.state.isOpen[] && subTreeDisplay}
+      </ol>
+    )
+  //    Object.keys(treeObj).map(k => {
+  //       let children = (Array.isArray(treeObj)) ?
+  //       (
+  //            treeObj.map(item => {
+  //             return (
+  //               <li>{item.title}</li>
+  //             )
+  //           })
+  //       )
+  //       : ( <TreeNode treeData={this.props.treeData[k]} label={k} /> )
+  //     return (
+  //       <ol>
+  //         <button styleName='header-toggleButton'
+  //               onMouseDown={(e) => this.handleToggleButtonClick(e)}
+  //             >
+  //               <img src={this.state.isOpen
+  //                 ? '../resources/icon/icon-down.svg'
+  //                 : '../resources/icon/icon-right.svg'
+  //                 }
+  //               />
+  //               {this.props.label}
+  //         </button>
+  //             {this.state.isOpen && children }
+  //       </ol>
+  //     )
+  //     })
+  // return (treeDisplay)
+  }
+  render () {
+    return (this.renderNode(this.props.label, this.props.treeData))
+  }
+}
 
 export default CSSModules(NoteTree, styles)
